34efa70d9afe6975792e0078e3c4b35537cb592a
Ana -rr <102273751+anatoly-rap@users.noreply.github.com>
2023-04-07 20:22:25 -0600

    Update Schedule..cc

--- Schedule.cc
+++ Schedule.cc
@@ -5,7 +5,7 @@
 using namespace std;
 
 Schedule::Schedule() {}
-Schedule::Schedule(const string& filename) {
+Schedule::Schedule(const string& filename){
     ifstream file(filename);
   if(file.is_open()){
     read(file);
@@ -16,51 +16,46 @@ Schedule::Schedule(istream& is) {
 }
 Schedule::Schedule(const Schedule& other) : events_(other.events_) {}
 
-Schedule& Schedule::operator=(const Schedule& other) {
+Schedule& Schedule::operator=(const Schedule& other){
   events_ = other.events_;
   return *this;
 }
-
 Schedule::~Schedule() {}
 
-void Schedule::read(istream& is) {
-  //clear();
+void Schedule::read(istream& is){
   string line;
   while (getline(is, line)) {
     events_.emplace_back(line.c_str());
     sort();
   }
 }
-void Schedule::clear() {
+void Schedule::clear(){
   events_.clear();
 }
-
 size_t Schedule::size() const {
   return events_.size();
 }
-
 bool Schedule::empty() const {
   return events_.empty();
 }
-void Schedule::sort() {
-    std::sort(events_.begin(), events_.end(), [](const Event& e1, const Event& e2) {
-        if (e1.year() < e2.year()) {
-            return true;
-        } else if (e1.year() > e2.year()) {
-            return false;
-        } else if (e1.month() < e2.month()) {
-            return true;
-        } else if (e1.month() > e2.month()) {
-            return false;
-        } else {
-            return e1.day() < e2.day();
+void Schedule::sort(){
+  std::sort(events_.begin(), events_.end(), [](const Event& e1, const Event& e2){
+    if(e1.year() < e2.year()){
+        return true;
+    }else if(e1.year() > e2.year()){
+        return false;
+    }else if(e1.month() < e2.month()){
+        return true;
+    }else if(e1.month() > e2.month()){
+        return false;
+      }else{
+        return e1.day() < e2.day();
         }
     });
 }
 const Event& Schedule::operator[](size_t index) const {
   return events_.at(index);
 }
-
 ostream& operator<<(ostream& os, const Schedule& schedule) {
     for (const auto& event : schedule.events_) {
         os << event << '\n';
