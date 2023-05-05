c72bdabe00f088083e39ddd743e65184843ec4e4
Ana -rr <102273751+anatoly-rap@users.noreply.github.com>
2023-05-05 17:43:38 -0600

    Update Event.h
    added iterator method signatures

--- Event.h
+++ Event.h
@@ -12,17 +12,27 @@ public:
     Event(const Event& other);
     Event& operator=(const Event& other);
     ~Event();
+    std::string fmt(const std::string& format = "%Y-%m-%d") const;
+    Event& operator++(); 
+    Event operator++(int);
+    Event& operator--();
+    Event operator--(int);
+    bool operator==(const Event& other) const;
+    bool operator!=(const Event& other) const;
+    bool operator<=(const Event& other) const;
+    bool operator>=(const Event& other) const;
+    bool operator<(const Event& other) const;
+    bool operator>(const Event& other) const;
     void set(int year, int month, int day);
     int year() const;
     int month() const;
     int day() const;
     std::string toString() const;
-    bool operator==(const Event& other) const;
 private:
-    void parseDate(const std::string& date);
     int m_year;
     int m_month;
     int m_day;
+    void parseDate(const std::string& date);
 };
 std::ostream& operator<<(std::ostream& os, const Event& event);
 #endif // EVENT_H
