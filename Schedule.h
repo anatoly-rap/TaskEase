4b24c3ce0275ad0f004fc189571a0f99caf97c10
Ana -rr <102273751+anatoly-rap@users.noreply.github.com>
2023-05-05 17:49:58 -0600

    Update Schedule.h
    added iterator signatures. Now have a const_iterator and regular iterator

--- Schedule.h
+++ Schedule.h
@@ -4,10 +4,17 @@
 #include <vector>
 #include <iosfwd>
 #include<algorithm>
+#include<iterator>
 #include "Event.h"
 
 class Schedule {
 public:
+    class iterator;
+    class const_iterator;
+    iterator begin();
+    iterator end();
+    const_iterator begin() const;
+    const_iterator end() const;
     Schedule();
     explicit Schedule(const std::string& filename);
     explicit Schedule(std::istream& is);
@@ -24,6 +31,41 @@ public:
 
 private:
     std::vector<Event> events_;
+};
+class Schedule::iterator {
+public:
+    iterator();
+    iterator(const iterator& other);
+    iterator(Schedule* schedule, std::size_t index);
+    iterator& operator++();
+    iterator operator++(int);
+    iterator& operator--();
+    iterator operator--(int);
+    const Event& operator*() const;
+    bool operator==(const iterator& other) const;
+    bool operator!=(const iterator& other) const;
+
+private:
+    Schedule* schedule_;
+    std::size_t index_;
+};
+class Schedule::const_iterator {
+public:
+    const_iterator();
+    const_iterator(const const_iterator& other);
+    const_iterator(const Schedule* schedule, std::size_t index);
+    const_iterator& operator++();
+    const_iterator operator++(int);
+    const_iterator& operator--();
+    const_iterator operator--(int);
+    const Event& operator*() const;
+    bool operator==(const const_iterator& other) const;
+    bool operator!=(const const_iterator& other) const;
+    operator iterator() const;
+
+private:
+    const Schedule* schedule_;
+    std::size_t index_;
 };
 // Output operator for Schedule
 std::ostream& operator<<(std::ostream& os, const Schedule& schedule);
